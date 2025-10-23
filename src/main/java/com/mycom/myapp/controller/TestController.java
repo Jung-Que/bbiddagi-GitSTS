c2fe12025f4a571cf8ee553c3be1367270c86d1c
Jung_Que <hahahajhs0529@gmail.com>
2025-10-23 14:24:10 +0900

    test

---
+++ src/main/java/com/mycom/myapp/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.mycom.myapp.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("Hello");
+        return "/hello.html";
+    }
+}
