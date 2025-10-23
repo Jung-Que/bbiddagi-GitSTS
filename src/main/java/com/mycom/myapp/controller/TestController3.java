94934f537797da09f83647565463b607b1dea39f
Jung_Que <hahahajhs0529@gmail.com>
2025-10-23 17:06:08 +0900

    testc2 수정

---
+++ src/main/java/com/mycom/myapp/controller/TestController3.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class TestController3 {
+    public void m(){
+    	
+    	System.out.println("TESTCONTORLLER2 - BEFORE()");
+    	System.out.println("TESTCONTORLLER2 - m()");
+    	System.out.println("TESTCONTORLLER2 - m()");
+    	System.out.println("TESTCONTORLLER2 - AFTER()");
+    }
+}
