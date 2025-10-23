6d4d233dc5dde8bf5545170d8274b416ccc46706
Jung_Que <hahahajhs0529@gmail.com>
2025-10-23 16:29:50 +0900

    t2 t4 add

---
+++ src/main/java/com/mycom/myapp/controller/TestController2.java
@@ -0,0 +1,12 @@
+package com.mycom.myapp.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class TestController2 {
+    public void m(){
+    	
+    	System.out.println("TESTCONTORLLER2 - BEFORE()");
+    	System.out.println("TESTCONTORLLER2 - m()");
+    	System.out.println("TESTCONTORLLER2 - AFTER()");
+    }
+}
---
+++ src/main/java/com/mycom/myapp/controller/TestController4.java
@@ -0,0 +1,12 @@
+package com.mycom.myapp.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class TestController4 {
+    public void m(){
+    	
+    	System.out.println("TESTCONTORLLER4 - BEFORE()");
+    	System.out.println("TESTCONTORLLER4 - m()");
+    	System.out.println("TESTCONTORLLER4 - AFTER()");
+    }
+}
